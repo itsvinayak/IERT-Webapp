265e53d336c5c5c5d500f34ab5bfe5255e8a18c1
itsvinayak <itssvinayak@gmail.com>
2019-10-26 10:35:09 +0530

    diploma admin

--- diploma/admin.py
+++ diploma/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import diploma_facultys
 
+# in case of different admin panel
+#####################################################
+# from django.contrib.admin import AdminSite
+#
+# class DiplomaAdminSite(AdminSite):
+#     site_header = "IERT DIPLOMA ADMIN"
+#     site_title = "IERT DIPLOMA ADMIN"
+#     index_title = "Welcome to IERT Diploma Admin Portal"
+#
+# diploma_admin_site = DiplomaAdminSite(name='iert_diploma_admin')
+########################################################################
+
 admin.site.register(diploma_facultys)
